44399ce5222e9143b67c79cf0eb493f847e07324
Jan Verweij <jan.verweij@liferay.com>
2020-11-04 08:57:53 +0100

    upgraded to 7.3

--- markdown-service/src/main/java/com/liferay/demo/webpackreacttutorial/markdown/service/MarkdownServiceImpl.java
+++ markdown-service/src/main/java/com/liferay/demo/webpackreacttutorial/markdown/service/MarkdownServiceImpl.java
@@ -1,6 +1,8 @@
 package com.liferay.demo.markdown.service;
 
 import com.liferay.demo.markdown.api.MarkdownService;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.vladsch.flexmark.util.data.MutableDataSet;
 import com.vladsch.flexmark.util.ast.Node;
 import com.vladsch.flexmark.html.HtmlRenderer;
@@ -21,21 +23,24 @@ import org.osgi.service.component.annotations.Component;
 public class MarkdownServiceImpl implements MarkdownService {
 	@Override
 	public String basicrenderer(String text) {
-		MutableDataSet options = new MutableDataSet();
+		_log.debug("Raw text: " + text);
 
+		MutableDataSet options = new MutableDataSet();
 		// uncomment to set optional extensions
 		//options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(), StrikethroughExtension.create()));
-
-		// uncomment to convert soft-breaks to hard breaks
 		options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
+
+		_log.debug("Options: " + options.toString());
 
 		Parser parser = Parser.builder(options).build();
 		HtmlRenderer renderer = HtmlRenderer.builder(options).build();
 
 		// You can re-use parser and renderer instances
 		Node document = parser.parse(text);
+		_log.debug("Parsed text: " + document);
 		return renderer.render(document);
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(MarkdownServiceImpl.class);
 
 }
